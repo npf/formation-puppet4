9008623885c31538d0f12198fad39b280bf6e643
Pierre Neyron <pierre.neyron@imag.fr>
2016-10-18 18:11:39 +0200

    corrigé tuto4

---
+++ part1/tuto4/manifests/tuto4.pp
@@ -0,0 +1,71 @@
+class apache {
+    package {
+      'apache2':
+        ensure => installed;
+    }
+    service {
+      'apache2':
+        ensure  => running,
+        enable  => true,
+        require => Package['apache2'];
+    }
+}
+class collectd (
+    $www = false
+) {
+    if $www == true {
+      include 'collectd::www'
+    }
+    package {
+      'collectd':
+        ensure => installed;
+    }
+    service {
+      'collectd':
+        ensure  => running,
+        enable  => true,
+        require => Package['collectd'];
+    }
+}
+class collectd::www {
+  include 'apache'
+  file {
+    '/etc/apache2/conf-available/collection3.conf':
+      ensure  => file,
+      mode    => '0644',
+      owner   => root,
+      group   => root,
+      source  => '/vagrant/tuto3/files/collection3.conf',
+      require => Package['apache2'],
+      notify  => Service['apache2'];
+  }
+  file {
+    '/etc/apache2/conf-enabled/collection3.conf':
+      ensure  => '/etc/apache2/conf-available/collection3.conf',
+      require => Package['apache2'],
+      notify  => Service['apache2'];
+  }
+  file {
+    '/etc/apache2/mods-enabled/cgid.conf':
+      ensure  => '/etc/apache2/mods-available/cgid.conf',
+      require => Package['apache2'],
+      notify  => Service['apache2'];
+    '/etc/apache2/mods-enabled/cgid.load':
+      ensure  => '/etc/apache2/mods-available/cgid.load',
+      require => Package['apache2'],
+      notify  => Service['apache2'];
+  }
+    package {
+      ['libconfig-general-perl',
+      'librrds-perl',
+      'libregexp-common-perl',
+      'libhtml-parser-perl']:
+        ensure => installed,
+        notify => Package['collectd'];
+    }
+}
+
+class {
+  'collectd':
+    www => true;
+}
